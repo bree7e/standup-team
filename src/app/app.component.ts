0b4d5b74edee7c008e58d5d1595eba05612b00fa
Alexandr Vetrov <aleksandr.vetrov@idp.zyfra.com>
2024-03-12 11:44:59 +0800

    feat: добавлен drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { NxWelcomeComponent } from './standup.component';
 
 @Component({
   standalone: true,
@@ -10,5 +10,5 @@ import { NxWelcomeComponent } from './nx-welcome.component';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'standup-team';
+  title = 'Daily Standup';
 }
