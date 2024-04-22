c2fb5316823644812e8dfccaca1c0237b23710c8
Alexandr Vetrov <aleksandr.vetrov@idp.zyfra.com>
2024-04-22 15:33:54 +0800

    fix: возвращает пустой роутер

---
+++ src/app/app.config.ts
@@ -0,0 +1,8 @@
+import { ApplicationConfig } from '@angular/core';
+import { Route, provideRouter } from '@angular/router';
+
+export const appRoutes: Route[] = [];
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(appRoutes)],
+};
